6546cf196fe7c955b0c431ec95e2be8e19ae647d
Organise4Good <65863470+Noe51@users.noreply.github.com>
2021-02-16 21:42:45 +0100

    add delete task function

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,10 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('check/', views.check, name='check'),
-    path('task/<str:pk>/', views.updateTask, name='task-details'),
+
+    path('task/update/<str:pk>/', views.updateTask, name='task-details'),
+    path('task/delete/<str:pk>/', views.deleteTask, name='delete'),
+
 
 
     path('clients', views.clients, name='clients'),
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from .models import Task, Client
 from .forms import TaskForm, ClientForm, FundForm
 from .filters import TaskFilter, ClientFilter
@@ -8,10 +8,11 @@ from .filters import TaskFilter, ClientFilter
 def home(request):
     current_analyst = request.user.analyst
     title = 'What do I have to do today?'
+
     if request.method == 'GET':
         my_tasks = Task.objects.filter(assignee=current_analyst)
         myFilter = TaskFilter(request.GET, queryset=my_tasks)
-        my_tasks = myFilter.qs
+        my_tasks = myFilter.qs.order_by('-date_created')
         form = TaskForm({'assignee':current_analyst})
         
 
@@ -24,8 +25,6 @@ def home(request):
             new_assignee.save()
             form.save()
             return redirect("/")
-
-    
 
     context={'title':title, 'my_tasks':my_tasks, 'form':form, 'myFilter':myFilter}
     return render(request, 'accounts/dashboard.html',context)
@@ -53,8 +52,9 @@ def check(request):
     return render(request, 'accounts/dashboard.html',context)
 
 def updateTask(request,pk):
-    task = Task.objects.get(id=pk)
-    form = TaskForm(instance=task)
+    if request.method == 'GET':
+        task = Task.objects.get(id=pk)
+        form = TaskForm(instance=task)
 
     if request.method == 'POST':
         form = TaskForm(request.POST, instance=task)
@@ -62,13 +62,21 @@ def updateTask(request,pk):
             form.save()
             return redirect("/")
     context={'form':form}
-    return render(request, 'accounts/task_details.html',context) 
+    return render(request, 'accounts/task_details.html',context)
+
+def deleteTask(request,pk):
+    if request.method == 'GET':
+        task = Task.objects.get(id=pk)
+        task.delete()
+        return redirect ('/')
+    return HttpResponse('error: could not delete task: '+task.id)
+
 
 def clients(request):
-    clients = Client.objects.all()
-
-    myFilter = ClientFilter(request.GET, queryset=clients)
-    clients = myFilter.qs
+    if request.method == 'GET':
+        clients = Client.objects.all()
+        myFilter = ClientFilter(request.GET, queryset=clients)
+        clients = myFilter.qs
     
     context={'clients':clients, 'myFilter':myFilter}
     return render(request, 'accounts/clients.html',context) 
@@ -79,27 +87,29 @@ def funds(request):
     return render(request, 'accounts/task_details.html',context) 
 
 def updateClient(request,pk):
-    # form = TaskForm()
-    # task = Task.objects.get(id=pk)
-    # form = TaskForm(instance=task)
-    # if request.method == 'POST':
-    #     form = TaskForm(request.POST)
-    #     if form.is_valid():
-    #         form = TaskForm(request.POST, instance=task)
-    #         form.save()
-    #         return redirect("/")
+    if request.method == 'GET':
+        client = Client.objects.get(id=pk)
+        form = ClientForm(instance=client)
+
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form = ClientForm(request.POST, instance=client)
+            form.save()
+            return redirect("/")
     context={'form':form}
     return render(request, 'accounts/task_details.html',context) 
 
 def updateFund(request,pk):
-    # form = TaskForm()
-    # task = Task.objects.get(id=pk)
-    # form = TaskForm(instance=task)
-    # if request.method == 'POST':
-    #     form = TaskForm(request.POST)
-    #     if form.is_valid():
-    #         form = TaskForm(request.POST, instance=task)
-    #         form.save()
-    #         return redirect("/")
+    if request.method == 'GET':
+        fund = Fund.objects.get(id=pk)
+        form = FundForm(instance=fund)
+        
+    if request.method == 'POST':
+        form = FundForm(request.POST)
+        if form.is_valid():
+            form = FundForm(request.POST, instance=fund)
+            form.save()
+            return redirect("/")
     context={'form':form}
     return render(request, 'accounts/task_details.html',context) 
